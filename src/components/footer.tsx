e002e3a2733f920568bdfab2c38336ad5c9284a0
JOSE ONGWAE <joseongwae6@gmail.com>
2025-06-26 13:50:28 +0000

    Make the footer date in this system keep updating itself based on year,

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Footer() {
     return (
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-            <p className="text-xs text-muted-foreground">&copy; 2024 Oriango MicroFinance. All rights reserved.</p>
+            <p className="text-xs text-muted-foreground">&copy; {new Date().getFullYear()} Oriango MicroFinance. All rights reserved.</p>
             <nav className="sm:ml-auto flex gap-4 sm:gap-6">
                 <Link href="/" className="text-xs hover:underline underline-offset-4">Home</Link>
                 <Link href="/terms-of-service" className="text-xs hover:underline underline-offset-4">Terms of Service</Link>
