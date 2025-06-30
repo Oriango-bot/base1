23f0e2889fe4a36bfaa330a576da0fd3f9a58194
JOSE ONGWAE <joseongwae6@gmail.com>
2025-06-30 09:47:33 +0000

    Still login fails. Even the signup fails

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,37 +2,32 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 
-// We need a lazy-loaded promise to avoid throwing an error at build time
-// if the environment variable isn't set.
+if (!uri) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env');
+}
+
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!uri) {
-  console.warn(
-    'Warning: MONGODB_URI environment variable is not defined. Database connections will fail at runtime.'
-  );
-  // Create a dummy promise that will be rejected if ever used.
-  // This allows the build to pass but will cause runtime errors if the DB is accessed.
-  clientPromise = Promise.reject(
-    new Error('Please define the MONGODB_URI environment variable')
-  );
-} else {
-    if (process.env.NODE_ENV === 'development') {
-    // In development mode, use a global variable so that the value
-    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+declare global {
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
+// A global variable is used to hold the client promise to avoid creating a new client
+// on every hot reload in development. This pattern is also safe in production.
+if (process.env.NODE_ENV === 'development') {
     if (!global._mongoClientPromise) {
         client = new MongoClient(uri, {
-        serverApi: {
+            serverApi: {
             version: ServerApiVersion.v1,
             strict: true,
             deprecationErrors: true,
-        }
+            }
         });
         global._mongoClientPromise = client.connect();
     }
     clientPromise = global._mongoClientPromise;
-    } else {
-    // In production mode, it's best to not use a global variable.
+} else {
     client = new MongoClient(uri, {
         serverApi: {
         version: ServerApiVersion.v1,
@@ -41,12 +36,7 @@ if (!uri) {
         }
     });
     clientPromise = client.connect();
-    }
 }
 
-
-declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
-}
 
 export default clientPromise;
